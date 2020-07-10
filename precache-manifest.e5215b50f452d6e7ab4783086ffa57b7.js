b8a67327fc58de1387babc69c1f40c3628def1e3
Johnatan Dias <johnatan.dias@gmail.com>
2020-07-09 22:41:34 -0300

    Updates

---
+++ precache-manifest.e5215b50f452d6e7ab4783086ffa57b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07aa0fad6af4227a765b3d19b927484c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9654729a3e1dd9742b",
+    "url": "/static/css/main.349b9415.chunk.css"
+  },
+  {
+    "revision": "fa98f3e086f115db4277",
+    "url": "/static/js/2.e20cc6b8.chunk.js"
+  },
+  {
+    "revision": "6f9654729a3e1dd9742b",
+    "url": "/static/js/main.38ac249c.chunk.js"
+  },
+  {
+    "revision": "7eaa768d5a3e0e377779",
+    "url": "/static/js/runtime~main.f89a94c3.js"
+  }
+]);
